ef3c68eab8261e3e8886159553da426a95685aee
Aryan Hamedani <aryn.hmd@gmail.com>
2022-12-08 21:52:04 +0330

    Orders app created and configured

---
+++ tento_shop_project/orders/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class OrdersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tento_shop_project.orders"
+    verbose_name = _("Orders")
+
+    def ready(self):
+        try:
+            import tento_shop_project.orders.signals  # noqa F401
+        except ImportError:
+            pass
