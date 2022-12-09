759d5a89740ff800bdd6724e8fbadd7c3289568b
Aryan Hamedani <aryn.hmd@gmail.com>
2022-12-09 03:55:02 +0330

    User app models implemented and registered in admin

--- tento_shop_project/users/models.py
+++ tento_shop_project/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -11,11 +11,6 @@ class User(AbstractUser):
     check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
 
-    #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
-
     def get_absolute_url(self):
         """Get url for user's detail view.
 
@@ -24,3 +19,19 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Profile(models.Model):
+    class Gender(models.IntegerChoices):
+        MALE = 0, _("Male")
+        FEMALE = 1, _("Female")
+        NONE = 2, _("None of binary")
+
+    owner = models.ForeignKey(
+        User, verbose_name=_("Profile Owner"), on_delete=models.CASCADE
+    )
+    first_name = models.CharField(_("First Name"), max_length=50, blank=False)
+    last_name = models.CharField(_("Last Name"), max_length=50, blank=False)
+    gender = models.IntegerField(
+        _("Gender"), choices=Gender.choices, blank=True, null=True
+    )
