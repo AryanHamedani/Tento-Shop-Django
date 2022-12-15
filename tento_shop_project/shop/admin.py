e3a6b4e6b1f8ec2c282cb6ae6f9ba47bf0fcdfad
Aryan Hamedani <aryn.hmd@gmail.com>
2022-12-16 02:55:14 +0330

    Category and Product models registered and customized for admin panel

--- tento_shop_project/shop/admin.py
+++ tento_shop_project/shop/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin  # noqa F401
+from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "price", "available", "created", "modified"]
+    list_filter = ["available", "created", "modified"]
+    list_editable = ["price", "available"]
+    prepopulated_fields = {"slug": ("name",)}
