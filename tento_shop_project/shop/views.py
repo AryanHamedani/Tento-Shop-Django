95200cb7465699500ba9c566439cef9f3e89595b
Aryan Hamedani <aryn.hmd@gmail.com>
2022-12-16 05:01:58 +0330

    Product list view implemented

--- tento_shop_project/shop/views.py
+++ tento_shop_project/shop/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render  # noqa F401
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from .models import Category, Product
+
+
+class ProductListView(generic.ListView):
+    template_name = "shop/product/list.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        self.category = None
+        self.categories = Category.objects.all()
+        self.products = Product.objects.filter(available=True)
+        if self.kwargs["category_slug"]:
+            self.category = get_object_or_404(
+                Category, slug=self.kwargs["category_slug"]
+            )
+            self.products = self.products.filter(
+                Q(category__in=self.category.sub_category.all())
+                | Q(category=self.category)
+            )
+        return self.products
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = self.categories
+        context["category"] = self.category
