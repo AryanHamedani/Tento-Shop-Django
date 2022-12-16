3aea520f24f5175bfda969378dc7dc2a12dfbee4
Aryan Hamedani <aryn.hmd@gmail.com>
2022-12-16 05:19:44 +0330

    shop app urls included in base url

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path("users/", include("tento_shop_project.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("shop/", include("tento_shop_project.shop.urls"), namespace="shop"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
