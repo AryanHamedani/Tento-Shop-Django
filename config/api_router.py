fa5d4788591e1929af5793e10e2ad2a4f92a3fe8
Aryan Hamedani <aryn.hmd@gmail.com>
2023-01-05 20:01:08 +0330

    Registering ProductViewSet in api_router.py

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from tento_shop_project.shop.api.views import ProductViewSet
 from tento_shop_project.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -9,6 +10,7 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("shop", ProductViewSet)
 
 
 app_name = "api"
