a5db1b0404edf8cd6f7a37b9f2478c2177e1159b
Aryan Hamedani <aryn.hmd@gmail.com>
2023-01-07 17:54:42 +0330

    fixing forms

--- tento_shop_project/users/forms.py
+++ tento_shop_project/users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.formfields import PhoneNumberField
 
 User = get_user_model()
 
@@ -8,6 +9,8 @@ User = get_user_model()
 class UserAdminChangeForm(admin_forms.UserChangeForm):
     class Meta(admin_forms.UserChangeForm.Meta):
         model = User
+        fields = "__all__"
+        field_classes = {"phone_number": PhoneNumberField}
 
 
 class UserAdminCreationForm(admin_forms.UserCreationForm):
@@ -18,6 +21,8 @@ class UserAdminCreationForm(admin_forms.UserCreationForm):
 
     class Meta(admin_forms.UserCreationForm.Meta):
         model = User
+        fields = ("phone_number",)
+        field_classes = {"phone_number": PhoneNumberField}
 
         error_messages = {
             "phone_number": {"unique": _("This phone_number has already been taken.")}
